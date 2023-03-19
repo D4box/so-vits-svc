0020b79583c01a3fcc20d5e720abdda40d59c44f
mmr <a2356960576@163.com>
2023-03-20 00:07:11 +0800

    Update inference_main.py

--- inference_main.py
+++ inference_main.py
@@ -2,28 +2,32 @@ import io
 import logging
 import time
 from pathlib import Path
-
+from IPython.display import Audio
 import numpy as np
 import soundfile
+import argparse
 
 from inference import infer_tool
 from inference import slicer
 from inference.infer_tool import Svc
 
 logging.getLogger('numba').setLevel(logging.WARNING)
+parser = argparse.ArgumentParser(description='sovits inference')
+parser.add_argument('-m', '--model_path', type=str, default="models/G_0.pth", help='模型路径')
+parser.add_argument('-c', '--config_path', type=str, default="models/config.json", help='配置文件路径')
+parser.add_argument('-n', '--clean_names', type=str, nargs='+', default=["君の知らない物語-src.wav"], help='wav文件名列表，放在raw文件夹下')
+parser.add_argument('-t', '--trans', type=int, nargs='+', default=[0], help='音高调整，支持正负（半音）')
+parser.add_argument('-s', '--spk_list', type=str, nargs='+', default=['nen'], help='合成目标说话人名称')
+args = parser.parse_args()
 
-model_path = "models/Trixie/G_22000.pth"
-config_path = "models/Trixie/config.json"
-svc_model = Svc(model_path, config_path)
+svc_model = Svc(args.model_path, args.config_path)
 infer_tool.mkdir(["raw", "results"])
 
-# 支持多个wav文件，放在raw文件夹下
-#clean_names = ["test1","test2","test3","test4"]
-clean_names=["trix1","trix2"]
-trans = [0]  # 音高调整，支持正负（半音）
-spk_list = ['trixie']  # 每次同时合成多语者音色
-slice_db = -40  # 默认-40，嘈杂的音频可以-30，干声保留呼吸可以-50
-wav_format = 'wav'  # 音频输出格式
+clean_names = args.clean_names
+trans = args.trans
+spk_list = args.spk_list
+slice_db = args.slice_db
+wav_format = args.wav_format
 
 infer_tool.fill_a_to_b(trans, clean_names)
 for clean_name, tran in zip(clean_names, trans):
@@ -53,3 +57,4 @@ for clean_name, tran in zip(clean_names, trans):
 
         res_path = f'./results/{clean_name}_{tran}key_{spk}.{wav_format}'
         soundfile.write(res_path, audio, svc_model.target_sample, format=wav_format)
+Audio(f'./results/{clean_name}_{tran}key_{spk}.{wav_format}')
