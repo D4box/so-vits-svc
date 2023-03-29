ab4fd08fc1b93c1617c48aa70b3590264d8ff3c4
mmr <a2356960576@163.com>
2023-03-30 01:51:09 +0800

    呃呃

--- resample.py
+++ resample.py
@@ -10,17 +10,16 @@ from tqdm import tqdm
 def process(item):
     spkdir, wav_name, args = item
     # speaker 's5', 'p280', 'p315' are excluded,
-    speaker = spkdir.replace("\\", "/").split("/")[-1]
+    speaker = spkdir.split(os.sep)[-1]
     wav_path = os.path.join(args.in_dir, speaker, wav_name)
     if os.path.exists(wav_path) and '.wav' in wav_path:
         os.makedirs(os.path.join(args.out_dir2, speaker), exist_ok=True)
         wav, sr = librosa.load(wav_path)
-        wav, _ = librosa.effects.trim(wav, top_db=45)
+        wav, _ = librosa.effects.trim(wav, top_db=20)
         peak = np.abs(wav).max()
         if peak > 1.0:
             wav = 0.98 * wav / peak
         wav2 = librosa.resample(wav, orig_sr=sr, target_sr=args.sr2)
-        wav2 /= max(wav2.max(), -wav2.min())
         save_name = wav_name
         save_path2 = os.path.join(args.out_dir2, speaker, save_name)
         wavfile.write(
@@ -33,9 +32,9 @@ def process(item):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--sr2", type=int, default=32000, help="sampling rate")
+    parser.add_argument("--sr2", type=int, default=48000, help="sampling rate")
     parser.add_argument("--in_dir", type=str, default="./dataset_raw", help="path to source dir")
-    parser.add_argument("--out_dir2", type=str, default="./dataset/32k", help="path to target dir")
+    parser.add_argument("--out_dir2", type=str, default="./dataset/48k", help="path to target dir")
     args = parser.parse_args()
     processs = cpu_count()-2 if cpu_count() >4 else 1
     pool = Pool(processes=processs)
